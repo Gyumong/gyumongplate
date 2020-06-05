561c364f47142bd373fd6eae1dd6a850a6920264
Gyumong <rlaalsrb3559@naver.com>
2020-06-06 02:03:59 +0900

    몽고 연결

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
+const {PORT, MONGO_URI} = process.env;
+
+mongoose
+    .connect(MONGO_URI,{useNewUrlParser:true,useFindAndModify:false})
+        .then(()=>{
+            console.log('MongoDB Connected...');
+        }).catch(e=>{
+            console.error(e);
+        });
+        
 const api =require('./api');
 
 const app = new Koa();
@@ -9,9 +23,12 @@ const router = new Router();
 // 라우터 설정 
 router.use('/api',api.routes()); // api 라우트 적용
 
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(6000, ()=>{
-    console.log('Listening to port 6000');
+const port = PORT||6000;
+app.listen(port, ()=>{
+    console.log('Listening to port %d',port);
 });
